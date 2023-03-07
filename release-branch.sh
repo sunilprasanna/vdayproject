2165d01b2497df782c22d83ce33c899bf0ffc448
Sunil Babu <91570349+sunilprasanna@users.noreply.github.com>
2023-03-07 15:31:49 -0600

    Update release-branch.sh

--- release-branch.sh
+++ release-branch.sh
@@ -6,20 +6,10 @@ export VERSION=$(cat version.txt)
 # Increment version number
 export RELEASE_VERSION=$(node -pe "require('semver').inc('${VERSION}', 'patch').trim()")
 
-# Get new version number from package.json
-export BRANCH_NAME=Version-${RELEASE_VERSION}
-
 echo "Version: ${RELEASE_VERSION}"
-echo "branch name: ${BRANCH_NAME}"
-
-# Delete local branch in case of duplicate. Ignore any errors
-git branch -D ${BRANCH_NAME} &>/dev/null
-
-# Delete Remote branch in case of duplicate. Ignore any errors
-git push origin --delete ${BRANCH_NAME} &>/dev/null
 
 # Create new branch and check it out
-git checkout -b ${BRANCH_NAME}
+git checkout -b development
 
 # Update version.txt
 echo ${RELEASE_VERSION} > version.txt
@@ -27,13 +17,7 @@ echo ${RELEASE_VERSION} > version.txt
 # Stage only version file, commit and push to remote
 git add version.txt
 git commit -m "chore: update to version ${RELEASE_VERSION}"
-git push -u origin ${BRANCH_NAME}
+git push -u origin development
 echo -e "\n\n\n🚂 Ready for departure 🚂 "
-
-export TEMPLATE="release_pull_request_template.md"
-export LABEL="release"
-export TARGET_BRANCH="release"
-
-echo "Opening Pull Request on Github..."
 
 exit 0
